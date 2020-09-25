7a97e1a724617d73cc2d1a7d703dc943df3d4c97
Cameron <noremackientz@gmail.com>
2020-09-24 19:01:16 -0500

    Now produces mouse input data whenever 3 fingers are on the touchpad. need to switch to condition vars instead of global bools

--- main.cpp
+++ main.cpp
@@ -7,87 +7,62 @@
 #include <mutex>
 #include <condition_variable>
 #include <cmath>
+#include <error.h>
 
 #include <cstring>
 #include <linux/input.h>
 
 #define MOUSEFILE "/dev/input/mice"
-#define EVENTFILE "/dev/input/event7"
+#define EVENTFILE "/dev/input/event6"
 
 //The higher this value is, the faster/longer you have to swipe
-#define SENSITIVITY 30
+#define SENSITIVITY 20
 
-// std::mutex mtx;
-// std::condition_variable cv;
-bool ready = false;
+bool tripleTap = false;
 
-// std::unique_lock<std::mutex> lck(mtx);
 int fds[2];
 char buf[3];
-
-int moveDistance_x;
 
 void printer(char data[]) {
   printf("%d, %d, %d\n", data[0], data[1], data[2]);
 }
 
-void action(std::string direction) {
-  char command[] = "xdotool\0";
-  char arg1[] = "key\0";
-  char argLeft[] = "ctrl+super+Right";
-  char argRight[] = "ctrl+super+Left";
-
-  if(direction == "Right") {
-    char* args[] = {command, arg1, argLeft, nullptr};
-    if(fork() == 0) {
-      execvp(command, args);
-      exit(1);
-    }
-  }
-  else if(direction == "Left") {
-    char* args[] = {command, arg1, argRight, nullptr};
-    if(fork() == 0) {
-      execvp(command, args);
-      exit(1);
-    }
-  }
-}
-
 void readMouseFile() {
   std::ifstream mouseFile (MOUSEFILE, std::ios::in | std::ios::binary);
 
-  char data[3];
-  std::string right = "Right";
-  std::string left = "Left";
-  size_t data_size = sizeof(data);
+  char mouseData[3];
+  size_t mouse_data_size = sizeof(mouseData);
+  int moveDistance_x = 0;
 
-  for(int i = 0; i < 10; i++) {
-    mouseFile.read(data, data_size);
-    moveDistance_x += data[1];
+  while(true) {
+    while(tripleTap) {
+      mouseFile.read(mouseData, mouse_data_size);
+      moveDistance_x = mouseData[1];
+      std::cout << moveDistance_x << std::endl;
+    }
   }
-  if(moveDistance_x > SENSITIVITY) {
-    action(left);
-    moveDistance_x = 0;
-  }
-  else if(moveDistance_x < (SENSITIVITY*-1)) {
-    action(right);
-    moveDistance_x = 0;
-  }
+  mouseFile.close();
 }
 
 void readEventFile() {
-  struct input_event event;
   std::ifstream eventFile(EVENTFILE);
 
-  while(eventFile.is_open()) {
-    char eventData[sizeof(event)];
-    eventFile.read(eventData, sizeof(event));
+  struct input_event event;
+  size_t eventSize = sizeof(event);
+  char eventData[eventSize];
 
-    memcpy(&event, eventData, sizeof(event));
-    if(event.type == EV_KEY) {
-      if(event.code == BTN_TOOL_TRIPLETAP) {
-        readMouseFile();
-        struct input_event event;
+  while(eventFile.is_open()) {
+    eventFile.read(eventData, eventSize);
+    memcpy(&event, eventData, eventSize);
+    if(event.code == BTN_TOOL_TRIPLETAP) {
+      tripleTap = true;
+      while(true) {
+        eventFile.read(eventData, eventSize);
+        memcpy(&event, eventData, eventSize);
+        if(event.code == BTN_TOOL_TRIPLETAP) {
+          tripleTap = false;
+          break;
+        }
       }
     }
   }
@@ -95,8 +70,11 @@ void readEventFile() {
 }
 
 
-
 int main(int argc, char** argv) {
-  readEventFile();
-	return 0;
+  std::thread eventReader (readEventFile);
+  std::thread mouseReader (readMouseFile);
+
+  eventReader.join();
+  mouseReader.join();
+  return 0;
 }
